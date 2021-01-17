8a61f1873159672ca66cd2d40b5de08b5a6c1b7a
AlinaSpahiu <alina.s.abbaz@gmail.com>
2021-01-17 12:56:31 +0100

    Update internet.page.js

--- pages/internet.page.js
+++ pages/internet.page.js
@@ -6,6 +6,7 @@ class Internet {
     get childElements() { return this.parent.$$('li')}
 
     specificChildElement(index) {return this.parent.$(`li:nth-child(${index})`)}
+    get firstLink() { return $('ul li:nth-child(1) a')}
 
     getLiText(){
         this.childElements.filter((element) => console.log(element.getText()))
@@ -14,5 +15,13 @@ class Internet {
     getSpecificElementText(index){
         console.log(this.specificChildElement(index).getText())
     }
+
+    clickOnLink(){
+     if ( this.firstLink.isDisplayed() === true) {
+         this.firstLink.click()
+     }
+     browser.pause(5000)
+    }
+
 }
 module.exports = new Internet()
