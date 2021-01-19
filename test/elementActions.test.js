250eaad03f755646fe1f02a034a68a61fa733e58
Alina Spahiu <alina.s.abbaz@gmail.com>
2021-01-19 20:59:57 +0100

    Update elementActions.test.js

--- test/elementActions.test.js
+++ test/elementActions.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai')
 const internetPage = require('../pages/internet.page')
 
+
 internetPage = require('../pages/internet.page')
 
 describe('Test element actions', function(){
@@ -8,5 +9,9 @@ describe('Test element actions', function(){
         browser.url('/')
         internetPage.clickOnLink()
         expect(browser.getUrl()).equals('http://the-internet.herokuapp.com/abtest')
+    })
+    it('should get text', ()=> {
+        browser.url('/')
+        expect(internetPage.getSpecificElementText(1)).equals('A/B Testing')
     })
 })
