c13323507456052365b3d57ee9ed1e12be7f76ad
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2021-01-23 23:52:52 +0100

    Create element.test.js

---
+++ test/specs/element.test.js
@@ -0,0 +1,32 @@
+// const { getPageHeader } = require('../pageobjects/Internet.page')
+// const internetPage = require('../pageobjects/Internet.page')
+// describe("Interacting with elements", () => {
+//     before(()=>{
+//         browser.url('/')
+//     })
+// // Get Text()
+//   xit('Get Text of header, footer, li', () =>{
+//       internetPage.getPageHeader();
+//       internetPage.getPageFooter();
+//       internetPage.getAllLi(); //getting length and text of all LI
+//       internetPage.getspecificLi(3);
+//   })
+
+//   xit("Click on link", ()=> {
+//     internetPage.clickOnLink(5);
+//     browser.url('/');
+//     internetPage.clickOnLink(6);
+//     browser.pause(2000);
+//   })
+
+//   // Element State
+//   xit('Element State', ()=>{
+//       console.log('pageHeader Displayed:', internetPage.pageHeader.isDisplayed())
+//       console.log('pageHeader Existing:', internetPage.pageHeader.isExisting())
+//       console.log('pageHeader Viewport:', internetPage.pageFooter.isDisplayedInViewport())
+//      // console.log('pageHeader Enabled:', internetPage.clickOnLink(4).isEnabled())
+//      // console.log('pageHeader Clickable:', internetPage.getIsDisplayed(5).isClickable())
+//   })
+
+// })
+
