9ca5250823372ed0d32fc1dd674e27552e9e27ac
Alina Spahiu <alina.s.abbaz@gmail.com>
2021-02-22 17:33:06 +0100

    Update Internet.page.js

--- test/pageobjects/Internet.page.js
+++ test/pageobjects/Internet.page.js
@@ -1,50 +1,61 @@
-class Internet {
-    get pageHeader() {return $('h1.heading')}
-    get pageFooter() {return $("#page-footer")}
-    get ul() { return $('ul')}
-    get lis() {return this.ul.$$("li")}
-    //nese dojm me punu me index nuk munemi me e perdor get
-     specificLi(index){return this.ul.$(`li:nth-child(${index}) a`)}
-    get specificLink(){return $(`ul li:nth-child(1) a`)}
-    
-   
+class internetPage {
 
-   getPageHeader(){
-       this.pageHeader.getText();
-       console.log("merrHeader", this.pageHeader.getText());
-   }
+    //pageHeader
+    get pageHeader() { return $('h1.heading') }
+    getPageHeader() {
+        this.pageHeader.getText();
+        console.log("merrHeader", this.pageHeader.getText());
+    }
 
-   getPageFooter(){
-    this.pageFooter.getText();
-    console.log("merrFooter", this.pageHeader.getText());
-   }
+    //pageFooter
+    get pageFooter() { return $("#page-footer") }
+    getPageFooter() {
+        this.pageFooter.getText();
+        console.log("merrFooter", this.pageHeader.getText());
+    }
 
-    getAllLi(){
+    //get All li-s 
+    get ul() { return $('ul') }
+    get lis() { return this.ul.$$("li") }
+    getAllLi() {
         this.lis.length;
         console.log("lengthi", this.lis.length);
 
-        this.lis.filter((element) => {
-            console.log('liiis', element.getText());
-         })
+        this.lis.filter((el) => {
+            console.log('liiis', el.getText());
+        })
     }
 
-    getspecificLi(index){
+
+    //nese dojm me punu me index nuk munemi me e perdor get
+    specificLi(index) { return this.ul.$(`li:nth-child(${index}) a`) }
+    get specificLink() { return $(`ul li:nth-child(1) a`) }
+
+
+
+
+
+
+
+
+
+    getspecificLi(index) {
         this.specificLi(index).waitForDisplayed()
         return this.specificLi(index).getText();
         browser.pause(3000);
     }
 
-    clickOnLink(){
-       if (this.specificLink.isDisplayed() === true) {
-          return this.specificLink.click()
-       }
-       browser.pause(5000)
+    clickOnLink() {
+        if (this.specificLink.isDisplayed() === true) {
+            return this.specificLink.click()
+        }
+        browser.pause(5000)
     }
 
-    getIsDisplayed(index){
+    getIsDisplayed(index) {
         console.log(this.specificLink(index))
     }
 
 }
 
-module.exports = new Internet()
+module.exports = new internetPage()
