c79213717445efdb83def2a4351cac8a06ef7bc0
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2021-03-26 23:49:16 +0100

    Update element.test.js

--- test/specs/element.test.js
+++ test/specs/element.test.js
@@ -21,7 +21,6 @@ describe("Interacting with elements", () => {
 //     internetPage.clickOnLink(6);
 //     browser.pause(2000);
 //   })
-
 //   // Element State
 //   xit('Element State', ()=>{
 //       console.log('pageHeader Displayed:', internetPage.pageHeader.isDisplayed())
